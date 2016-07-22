4cf155e0e7763a5714caa9f373bb37b807cd3eb0
liusongyang <liusong.yang@nationwide.com>
2016-07-21 21:39:26 -0400

    added only two lines of import for testing

--- FlaskWebProject/views.py
+++ FlaskWebProject/views.py
@@ -8,9 +8,8 @@ from datetime import datetime
 from flask import render_template, jsonify
 from FlaskWebProject import app
 from flask import request
-import json
 import pandas as pd
-
+import json
 points = [
     {
         'time': 1,
@@ -36,32 +35,17 @@ def test():
            ]
     return jsonify(results = list)
 
-def df_to_json(y):
-    j = "["
-    for i in xrange(len(y)):
-        line=y.loc[i,:]
-        #item={"flag": line[0],"time": line[1],"temp":line[2]}
-        item='{\"'+str(y.columns[0])+'\": ' +str(line[0])+', \"'+str(y.columns[1])+'\": '+ str(line[1])+', \"'+str(y.columns[2])+'\": '+str(line[2])+'}'
-        #j.append(item)
-        if i == 0:
-            j=j+item
-        else:
-            j = j + ", " + item
-                
-    j=j+']'
-    return j
 	
 @app.route('/ml', methods=['POST'])
 def doML():
-    inpt=u"[{\"flag\": \"False\", \"time\": 0,\"temp\":\"60\"},{\"flag\": \"False\", \"time\": 1,\"temp\":\"62\"}]"
-
-    data_jason=json.loads(inpt)
- 
-    data=pd.DataFrame(data_jason)
-   
-    data_jason_flagged = df_to_json(data)
-    
-    return data_jason_flagged
-
+    if not request.json or not 'temp' in request.json:
+        abort(400)
+    temp = {
+        'id': points[-1]['id'] + 1,
+        'temp': request.json['temp'],
+        'done': True
+    }
+    points.append(temp)
+    return jsonify({'temp': temp}), 201
 
 
