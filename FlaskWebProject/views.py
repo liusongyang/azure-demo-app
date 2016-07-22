ccbe769468b265fef2b3cab91098cddc12ca9d80
liusongyang <liusong.yang@nationwide.com>
2016-07-21 21:33:09 -0400

    rolled back the first ml draft

--- FlaskWebProject/views.py
+++ FlaskWebProject/views.py
@@ -8,6 +8,8 @@ from datetime import datetime
 from flask import render_template, jsonify
 from FlaskWebProject import app
 from flask import request
+import json
+import pandas as pd
 
 points = [
     {
@@ -39,7 +41,7 @@ def df_to_json(y):
     for i in xrange(len(y)):
         line=y.loc[i,:]
         #item={"flag": line[0],"time": line[1],"temp":line[2]}
-        item='{\"'+str(col[0])+'\": ' +str(line[0])+', \"'+str(col[1])+'\": '+ str(line[1])+', \"'+str(col[2])+'\": '+str(line[2])+'}'
+        item='{\"'+str(y.columns[0])+'\": ' +str(line[0])+', \"'+str(y.columns[1])+'\": '+ str(line[1])+', \"'+str(y.columns[2])+'\": '+str(line[2])+'}'
         #j.append(item)
         if i == 0:
             j=j+item
@@ -51,15 +53,15 @@ def df_to_json(y):
 	
 @app.route('/ml', methods=['POST'])
 def doML():
-    if not request.json or not 'temp' in request.json:
-        abort(400)
-    temp = {
-        'id': points[-1]['id'] + 1,
-        'temp': request.json['temp'],
-        'done': True
-    }
-    points.append(temp)
-    return jsonify({'temp': temp}), 201
+    inpt=u"[{\"flag\": \"False\", \"time\": 0,\"temp\":\"60\"},{\"flag\": \"False\", \"time\": 1,\"temp\":\"62\"}]"
+
+    data_jason=json.loads(inpt)
+ 
+    data=pd.DataFrame(data_jason)
+   
+    data_jason_flagged = df_to_json(data)
+    
+    return data_jason_flagged
 
 
 
